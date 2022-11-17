15d75d90f0f3f8b13544a43dd590ec7dfbadc590
Michael Wimble <mike@wimblerobotics.com>
2022-11-17 10:39:06 -0800

    Fix compiler warnings

--- tm5/troboclaw.cpp
+++ tm5/troboclaw.cpp
@@ -23,7 +23,7 @@ void TRoboClaw::CheckForMotorStall() {
 
   if (abs(motor_current = GetM1Current()) > kStallCurrentThreshold) {
     consecutiveStallFaultsLeftMotor += 1;
-    snprintf(msg, sizeof(msg), "WARN [TRoboclaw::CheckForMotorStall] M1 overcurrent: %f, consecutive faults: %d",
+    snprintf(msg, sizeof(msg), "WARN [TRoboclaw::CheckForMotorStall] M1 overcurrent: %f, consecutive faults: %ld",
     motor_current, consecutiveStallFaultsLeftMotor);
     TMicroRos::singleton().PublishDiagnostic(msg);
   } else {
@@ -32,7 +32,7 @@ void TRoboClaw::CheckForMotorStall() {
 
   if (abs(motor_current = GetM2Current()) > kStallCurrentThreshold) {
     consecutiveStallFaultsRightMotor += 1;
-    snprintf(msg, sizeof(msg), "WARN [TRoboclaw::CheckForMotorStall] M2 overcurrent: %f, consecutive faults: %d",
+    snprintf(msg, sizeof(msg), "WARN [TRoboclaw::CheckForMotorStall] M2 overcurrent: %f, consecutive faults: %ld",
     motor_current, consecutiveStallFaultsRightMotor);
     TMicroRos::singleton().PublishDiagnostic(msg);
   } else {
